e3751b9ac710f633afe41fda0555fbdaad9c7e49
jarbasai <jarbasai@mailfence.com>
2023-03-25 03:54:36 +0000

    automations

--- scripts/bump_major.py
+++ scripts/bump_major.py
@@ -2,7 +2,7 @@ import fileinput
 from os.path import join, dirname
 
 
-version_file = join(dirname(dirname(__file__)), "ovos_solver_rivescript", "version.py")
+version_file = join(dirname(dirname(__file__)), "ovos_solver_rivescript_plugin", "version.py")
 version_var_name = "VERSION_MAJOR"
 minor_var_name = "VERSION_MINOR"
 build_var_name = "VERSION_BUILD"
--- scripts/remove_alpha.py
+++ scripts/remove_alpha.py
@@ -2,7 +2,7 @@ import fileinput
 from os.path import join, dirname
 
 
-version_file = join(dirname(dirname(__file__)), "ovos_solver_rivescript", "version.py")
+version_file = join(dirname(dirname(__file__)), "ovos_solver_rivescript_plugin", "version.py")
 
 alpha_var_name = "VERSION_ALPHA"
 
